5cf6ebca170915174c383d4018b34e536a817bc8
SergiObiols <sobiolslleixa@gmail.com>
2026-03-15 11:23:27 +0100

    Modificacion estilos

---
+++ contacto.js
@@ -0,0 +1,6 @@
+emailjs.init('PTdadKHmSKKiMcfiL');        // Account > API Keys
+emailjs.sendForm(
+  'service_icnx8kr',                    // Email Services > Service ID
+  'template_4i57nyd',                   // Email Templates > Template ID
+  this
+)
